91d8a6fe8bdd6fa487f894dd7e1daab2324297e1
IvanChi <ichirkov95@yandex.ru>
2024-09-25 19:42:35 +0400

    Addet Lesson4

---
+++ Lesson4/Task2/Program.cs
@@ -0,0 +1,84 @@
+﻿// Задача №2
+// • Назовём число «интересным» если его сумма цифр чётная
+// • Создать двумерный массив, состоящий из целых чисел. Вывести на экран «интересные» элементы массива
+
+
+
+// скопировали функцию из предыдущего задания для создания двумерного массива из рандомных чисел и последующим выводом его. 
+// изменили выборку: от 1 до 1000
+
+int[,] CreateMatrix(int rowCount, int columsCount)
+{
+    int[,] matrix = new int[rowCount, columsCount];
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 1000);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// команда для ввывода двумерного массива состоящего из 3 строк и 4 цифр
+
+int[,] matrix = CreateMatrix(3, 4);
+
+// вывод массива в консоль
+
+ShowMatrix(matrix);
+
+// создаем цикл для перебора элементов массива для выявления "интересности"
+
+foreach (int e in matrix)
+{
+    if (IsInteresting(e) == true) // функция IsInterestin создана ниже
+    {
+        Console.WriteLine(e);
+    }
+}
+
+// создаем функцию IsInteresting для выявления суммы цифр числа (int value) на четность 
+
+bool IsInteresting(int value)
+{
+    int sumOfDigit = GetSumOfDigit(value); // сумма цифр числа (int value) определяется функцией GetSumOfDigit (создана ниже)
+    if (sumOfDigit % 2 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+}
+
+// создаем функцию GetSumOfDigit 
+
+int GetSumOfDigit(int value)
+{
+    int sum = 0;
+    while (value > 0)
+    {
+        sum = sum + value % 10; // нахождение первой части числа
+        value = value / 10; // нахождение следующей части числа
+    }
+    return sum;
+}
+
---
+++ Lesson4/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача №3
+// Считать с консоли строку, состоящую из цифр и латинских букв.
+// Сформировать новую строку, состоящую из букв исходной строки.
+
+//создаем функцию для перебора и поиска в строке (string s) латинских букв
+string GetLettersFromString(string s)
+{
+    string letters = ""; // ввод пустой строки
+    foreach (char e in s)
+    {
+        if (char.IsAsciiLetter(e) == true) // char.IsAsciiLetter встроенная функция поиска латинских букв
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+string str = Console.ReadLine(); //команда для ввода строки с консоли
+
+string resalt = GetLettersFromString(str);
+Console.WriteLine(resalt); //команда для вывода результата в консоль выполнения функции GetLettersFromString(str)
